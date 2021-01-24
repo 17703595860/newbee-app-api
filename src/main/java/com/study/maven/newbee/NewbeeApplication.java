6c723277615d2e2a9d8e86ce3176f67ffd7f1bd5
helinhai <17703595860@163.com>
2021-01-24 11:24:23 +0800

    init

---
+++ src/main/java/com/study/maven/newbee/NewbeeApplication.java
@@ -0,0 +1,20 @@
+package com.study.maven.newbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springBoot启动器
+ *
+ * @author HLH
+ * @version 1.0
+ * @email 17703595860@163.com
+ */
+@SpringBootApplication
+public class NewbeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewbeeApplication.class, args);
+    }
+
+}
